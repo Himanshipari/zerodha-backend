792566766e695aca18ad3847d07d88b0bb8635be
himanshi <pariharhimanshi02@gmail.com>
2026-03-15 19:41:49 +0530

    backend Updated

---
+++ model/UserModel.js
@@ -0,0 +1,12 @@
+const { model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const UserSchema = require("../schemas/UserSchema");
+
+UserSchema.pre("save", async function () {
+    if(this.isModified("password")){
+  this.password = await bcrypt.hash(this.password, 12);
+    }
+});
+
+module.exports = model("User", UserSchema);
