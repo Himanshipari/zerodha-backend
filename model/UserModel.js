934e829f288786019f04985ce92fbd88fc4d09a7
himanshi <pariharhimanshi02@gmail.com>
2026-06-27 15:37:07 +0530

    fix

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,29 +1,3 @@
-// const { model } = require("mongoose");
-// const bcrypt = require("bcryptjs");
-
-// const UserSchema = require("../schemas/UserSchema");
-
-// UserSchema.pre("save", async function () {
-//     if(this.isModified("password")){
-//   this.password = await bcrypt.hash(this.password, 12);
-//     }
-// });
-
-// module.exports = model("User", UserSchema);
-
-// const { model } = require("mongoose");
-// const bcrypt = require("bcryptjs");
-
-// const UserSchema = require("../schemas/UserSchema");
-
-// UserSchema.pre("save", async function () {
-//     if(this.isModified("password")){
-//   this.password = await bcrypt.hash(this.password, 12);
-//     }
-// });
-
-// module.exports = model("User", UserSchema);
-
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const UserSchema = require("../schemas/UserSchema");
--- schemas/UserSchema.js
+++ schemas/UserSchema.js
@@ -1,28 +1,3 @@
-// const { Schema } = require("mongoose");
-
-
-// const UserSchema = new Schema({
-//   email: {
-//     type: String,
-//     required: [true, "Your email address is required"],
-//     unique: true,
-//   },
-//   username: {
-//     type: String,
-//     required: [true, "Your username is required"],
-//   },
-//   password: {
-//     type: String,
-//     required: [true, "Your password is required"],
-//   },
-//   createdAt: {
-//     type: Date,
-//     default: new Date(),
-//   },
-// });
-
-// module.exports = UserSchema;
-
 const { Schema } = require("mongoose");
 
 const UserSchema = new Schema({
